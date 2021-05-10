13ee6005d0fe9248d46ccb7e95e4f08812348d0b
varsubham <var.subham@gmail.com>
2021-05-10 19:05:03 +0530

    add logic in notifyPriceChange endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const verifyUser = require("./verifyUser");
 const bodyParser = require("body-parser");
+const { stopPriceCheck, startPriceCheck } = require("./bitbns_api");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -16,13 +17,7 @@ app.use(bodyParser.json());
 
 // endpoints
 
-/**
- * Returns x raised to the n-th power.
- *
- * @param {number} x The number to raise.
- * @param {number} n The power, must be a natural number.
- * @return {number} x raised to the n-th power.
- */
+// get notification if price hit certain value
 app.post("/api/notifyPriceChange", (req, res) => {
   const user = {
     username: req.body.username,
@@ -32,11 +27,30 @@ app.post("/api/notifyPriceChange", (req, res) => {
   if (!verifyUser(user, res)) {
     return;
   }
+  const crypto = req.body.crypto;
+  const price_to_hit = req.body.price_to_hit;
+  if (!crypto || !price_to_hit) {
+    res.json({
+      success: false,
+      message: "provide crypto coin and price_to_hit",
+    });
+    return;
+  }
+  // start price check
+  startPriceCheck(crypto, price_to_hit);
 
-  // send something
+  // assuming startPriceCheck function started without
+  // any problem.
+  // TODO: find a better approach here
+  res.send("started");
+});
+
+// stop price check
+app.post("/api/stopPriceCheck", (req, res) => {
+  stopPriceCheck();
   res.json({
     success: true,
-    message: "hello from express",
+    message: "price check stopped",
   });
 });
 
